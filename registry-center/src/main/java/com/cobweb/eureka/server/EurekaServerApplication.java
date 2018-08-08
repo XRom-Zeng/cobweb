8be6fbb939edda329220af7acacd275a63eeef97
XRom <18381005946@163.com>
2018-08-08 18:19:47 +0800

    add module: registry-center(eureka server)

---
+++ registry-center/src/main/java/com/cobweb/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.cobweb.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: XRom
+ * @createdTime: 2018-08-08 17:31:59
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
