5ce8ba9c0d05982294d5514cf9e5e9c62faf6c01
XRom <18381005946@163.com>
2018-09-13 17:12:22 +0800

    module(demo):编写kafka测试代码

--- demo/src/main/java/com/cobweb/xrom/demo/DemoApplication.java
+++ demo/src/main/java/com/cobweb/xrom/demo/DemoApplication.java
@@ -1,19 +1,62 @@
 package com.cobweb.xrom.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
-/**
- * @author: XRom
- * @email: XRom.Zeng@outlook.com
- * @createdTime: 2018-09-01 17:37:23
- */
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @SpringBootApplication
-@EnableKafka
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+    public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args).close();
     }
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    private final CountDownLatch latch = new CountDownLatch(3);
+
+    @Override
+    public void run(String... args) throws Exception {
+        oracle();
+    }
+
+    public void oracle() throws Exception {
+        Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
+    }
+
+    public void kafka() throws Exception {
+        this.template.send("test", "foo1");
+        this.template.send("test", "foo2");
+        ListenableFuture<SendResult<String, String>> send = this.template.send("test", "foo3");
+
+        send.addCallback(o -> log.info("success"),o -> log.error("failure"));
+        SendResult<String, String> stringStringSendResult = send.get();
+        latch.await(60, TimeUnit.SECONDS);
+        logger.info("All received");
+    }
+
+    @KafkaListener(topics = "test")
+    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+        logger.info(cr.toString());
+        latch.countDown();
+    }
+
 }
